f94b1de664c2d36e637b63e3e9ad840bb167b25f
freemany <freeman_yam@hotmail.com>
2019-03-26 15:09:12 +1100

    master content

--- src/config/slideshow.js
+++ src/config/slideshow.js
@@ -1,6 +1,7 @@
 export default [
     {
         content: `
+<h4>Set up first</h4>
 <h5>Install npm dependencies and phpunit:</h5>
 <pre><code class="bash">
 $ npm i
@@ -14,19 +15,29 @@ $ php vendor/bin/phpunit
     },
     {
         content: `
-<h5>Code here:</h5>
-<pre><code clas="php">        
-class PersonTest extends TestCase
-{
-     public function testPush()
-     {
-         $person = new Person();
+<h4>Testable testable and testable</h4>
+<p>The most important element for unit test is to have a testable code first, without testable, there is no way or no point to test because it will
+involves too much resources for the no-ready-for-test/none-testable codes. In order to write testable codes, in PHP we have to embrace the OOP patterns which sticks
+to the SOLID principal</p>
+<p>"In object-oriented computer programming, SOLID is a mnemonic acronym for five design principles intended to make software designs more understandable, flexible and maintainable..."  <a href="https://en.wikipedia.org/wiki/SOLID" target="_blank">-- by Wikipedia</a></p> 
+<ul>
+<li>Single responsibility principle:<br/>
+A class should have only a single responsibility, that is, only changes to one part of the software's specification should be able to affect the specification of the class.</li>
+<li>Open–closed principle:<br/>
+"Software entities ... should be open for extension, but closed for modification."</li>
+<li>Liskov substitution principle:<br/>
+"Objects in a program should be replaceable with instances of their subtypes without altering the correctness of that program." See also design by contract.</li>
+<li>Interface segregation principle:<br/>
+"Many client-specific interfaces are better than one general-purpose interface."</li>
+<li>Dependency inversion principle: <br/>
+One should "depend upon abstractions, [not] concretions."</li>
+</ul>
+`},  // 1
+    { content:`
 
-         $this->assertTrue($person->push());
-     }
-}
-</code></pre>       
+<p>The 'Single responsibility principle' aka 'Separation of Concern' is the most foundamental principal to DRY(Don't Repeat Yourself) code</p>
+<p>Lets git-checkout to 'tutorial-1' and start our first code and test and have fun with TDD !!!</p>
 `
-    } // 1
+    } // 2
 
 ];
